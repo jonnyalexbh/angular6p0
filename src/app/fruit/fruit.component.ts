0a2ef7e081ecc7e84c3e3af6dc384019c0d100ce
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-05-11 14:02:58 -0500

    properties and data types

--- src/app/fruit/fruit.component.ts
+++ src/app/fruit/fruit.component.ts
@@ -4,24 +4,20 @@ import { Component } from '@angular/core';
   selector: 'fruit',
   templateUrl: './fruit.component.html'
 })
-
-/*
-  @Component({
-    selector: 'fruit',
-    template: `
-    <h2>{{ name_component }}</h2>
-    <p>{{ list_fruit }}</p>
-    `
-})
-*/
-
 /**
 * FruitComponent
 *
 */
 export class FruitComponent {
-
+  
   public name_component = 'Fruit Component';
   public list_fruit = 'Apple, banana, mango and pear';
+  private vprivate = 'private variables do not work in javaScript';
+
+  public name:string = 'jonnyalexbh';
+  public age:number = 26;
+  public adult:boolean = true;
+  public jobs:Array<string> = ['Carpenter', 'bricklayer', 'plumber'];
+  public anyone:any = 99;
 
 }
