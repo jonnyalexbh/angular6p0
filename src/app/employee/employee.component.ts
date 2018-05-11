6d348819c74cb0ebb75350fb37ceef88c1de1a86
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-05-11 16:16:42 -0500

    create classes, data models, and objects

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,16 +1,43 @@
 import { Component } from '@angular/core';
-
+import { Employee } from './employee';
+/**
+* decorator
+*
+*/
 @Component({
   selector: 'employee-tag',
   templateUrl: './employee.component.html'
 })
-
 /**
 * EmployeeComponent
 *
 */
 export class EmployeeComponent {
 
-  public title = 'Title of the component employee';
+  public title = 'Component employee';
+  public employee:Employee;
+  public workers:Array<Employee>;
+  /**
+  * constructor
+  *
+  */
+  constructor()
+  {
+    this.employee = new Employee('Jonny Alexander', 27, 'Web developer', true);
+    this.workers = [
+      new Employee('Jonny Alexander', 26, 'Ing. sistemas', true),
+      new Employee('Juan David Lopez', 30, 'Ing. sistemas', false),
+      new Employee('Daniela Lopez', 56, 'Ing. industrial', true)
+    ];
+  }
+  /**
+  * ngOnInit - first method that is executed after the constructor
+  *
+  */
+  ngOnInit()
+  {
+    console.log(this.employee);
+    console.log(this.workers);
+  }
 
 }
---
+++ src/app/employee/employee.ts
@@ -0,0 +1,24 @@
+export class Employee {
+  /*
+  public name:string;
+  public age:number;
+
+  constructor(name, age){
+  this.name = name;
+  this.age = age;
+}
+*/
+
+/**
+* constructor
+*
+*/
+constructor(
+  public name:string,
+  public age:number,
+  public position:string,
+  public hired:boolean
+){
+}
+
+}
