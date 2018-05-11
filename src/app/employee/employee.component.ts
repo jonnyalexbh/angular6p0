42cde86a0402e3bc6d4ce06c0132a2358c89c779
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-05-11 13:08:29 -0500

    exercise and multiple components

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'employee-tag',
+  templateUrl: './employee.component.html'
+})
+
+/**
+* EmployeeComponent
+*
+*/
+export class EmployeeComponent {
+
+  public title = 'Title of the component employee';
+
+}
