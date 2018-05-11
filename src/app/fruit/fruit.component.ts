6b2067ff30550ba5ba9908a131277776363405af
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-05-11 14:29:49 -0500

    the constructor, one-component methods and NgOnInit

--- src/app/fruit/fruit.component.ts
+++ src/app/fruit/fruit.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+/**
+* decorator
+*
+*/
 @Component({
   selector: 'fruit',
   templateUrl: './fruit.component.html'
@@ -9,15 +12,46 @@ import { Component } from '@angular/core';
 *
 */
 export class FruitComponent {
-  
+
   public name_component = 'Fruit Component';
   public list_fruit = 'Apple, banana, mango and pear';
   private vprivate = 'private variables do not work in javaScript';
 
-  public name:string = 'jonnyalexbh';
-  public age:number = 26;
-  public adult:boolean = true;
+  public name:string;
+  public age:number;
+  public adult:boolean;
   public jobs:Array<string> = ['Carpenter', 'bricklayer', 'plumber'];
-  public anyone:any = 99;
+  public anyone:any;
+  /**
+  * constructor
+  *
+  */
+  constructor()
+  {
+    this.name = 'jonnyalexbh';
+    this.age = 27;
+    this.adult = true;
+    this.anyone = 'Yes';
+  }
+  /**
+  * ngOnInit - first method that is executed after the constructor
+  *
+  */
+  ngOnInit()
+  {
+    this.changeName(this.name);
+    this.changeAge(this.age);
+  }
+  /**
+  * changeName - changeAge
+  *
+  */
+  changeName(name) {
+    console.log("My name is " + name);
+  }
+
+  changeAge(age) {
+    console.log("My age is " + age);
+  }
 
 }
