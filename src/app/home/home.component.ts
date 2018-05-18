869963fe3420cabfedfbe3975a4b3298cbaf74ea
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-05-18 10:29:00 -0500

    exercise with services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,10 @@ import { ClothesService } from '../services/clothes.services';
 *
 */
 export class HomeComponent {
+
   public title = 'Homepage';
+  public clothing_list:Array<string>;
+  public hold_save:string;
   /**
   * constructor
   *
@@ -29,7 +32,22 @@ export class HomeComponent {
   *
   */
   ngOnInit(){
-    this._clothesService.test('Jean');
-    console.log(this._clothesService.test('Jean Colombia'));
+    this.clothing_list = this._clothesService.getClothes();
+    console.log(this.clothing_list);
+  }
+  /**
+  * savePledge
+  *
+  */
+  savePledge(){
+    this._clothesService.addClothes(this.hold_save);
+    this.hold_save = null;
+  }
+  /**
+  * removeGarment
+  *
+  */
+  removeGarment(index:number){
+    this._clothesService.removeGarment(index);
   }
 }
